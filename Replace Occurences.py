c97de1aca014888fdf94ed39388073d23e58ab91
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-05-27 22:40:50 +0800

    Create Replace Occurences.py

---
+++ Replace Occurences.py
@@ -0,0 +1,10 @@
+word = input("Input String : ")
+toBeReplaced = input("Input a the part of the string you want to be replaced : ")
+
+print(word.replace(toBeReplaced, len(toBeReplaced)*"*"))
+    
+
+
+
+
+            
